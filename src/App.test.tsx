18090190bc63d355e1c2fd894bf94a47cdbe4fa3
Alexandre dos Santos Gonçalves <alexandredossantosgoncalves@MacBook-Air-de-Alexandre.local>
2026-06-19 19:47:44 -0300

    test: update App test to assert heading

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App Component', () => {
   test('should render without crashing', () => {
     render(<App />);
-    const links = screen.getAllByRole('link');
-    expect(links.length).toBeGreaterThan(0);
+    const heading = screen.getByRole('heading', {name: /Tailwind v4 Configurado!/i});
+    expect(heading).toBeDefined();
   });
 });
